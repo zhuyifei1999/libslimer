7a1cc7ec5936ac9ade8d797d6afcf1e72e215f6e
zhuyifei1999 <zhuyifei1999@gmail.com>
2018-09-17 14:05:57 -0500

    Add dragging and scroll-less rand coords

--- guest/domutils.js
+++ guest/domutils.js
@@ -2,6 +2,18 @@ const scrollTo = require( './scrollTo.js' ),
 	statistics = require( './statistics.js' );
 
 module.exports = {
+	cloneDOMRect( rect ) {
+		return {
+			top: rect.top,
+			right: rect.right,
+			bottom: rect.bottom,
+			left: rect.left,
+			width: rect.width,
+			height: rect.height,
+			x: rect.x,
+			y: rect.y
+		};
+	},
 	closest( e, selector ) {
 		while ( true ) {
 			e = e.parentNode;
@@ -13,25 +25,52 @@ module.exports = {
 			}
 		}
 	},
-	randomCoords( e, m = 0.5, s = 0.25, check = true ) {
+	randomCoords( e, m = 0.5, s = 0.25, check = true, scroll = true ) {
 		if ( typeof m === 'number' ) { m = [ m, m ]; }
 		if ( typeof s === 'number' ) { s = [ s, s ]; }
 
 		let [ mx, my ] = m,
-			[ sx, sy ] = s;
+			[ sx, sy ] = s,
+			// We must consider the scrollbar size of around 20 pixels
+			scrollbarSize = 20,
+			rect = this.cloneDOMRect( e.getBoundingClientRect() );
+
+		if ( !scroll ) {
+			rect.left = Math.max( 0, rect.left );
+			rect.top = Math.max( 0, rect.top );
+			rect.right = Math.min( window.innerWidth - scrollbarSize, rect.right );
+			rect.bottom = Math.min( window.innerHeight - scrollbarSize, rect.bottom );
+
+			if ( rect.right <= rect.left || rect.bottom <= rect.top ) {
+				throw new Error( 'Unable to find coords for the element' );
+			}
+		}
+
+		rect.width = rect.right - rect.left;
+		rect.height = rect.bottom - rect.top;
+		rect.x = rect.left;
+		rect.y = rect.top;
+
 		for ( let i = 0; i < 16; i++ ) {
-			let rect = e.getBoundingClientRect(),
-				[ vx, vy ] = statistics.randomBoxMuller(),
-				x = rect.x + mx * rect.width + sx * vx * rect.width,
-				y = rect.y + my * rect.height + sy * vy * rect.height,
+			let [ vx, vy ] = statistics.randomBoxMuller(),
+				x = rect.left + mx * rect.width + sx * vx * rect.width,
+				y = rect.top + my * rect.height + sy * vy * rect.height,
 				found;
 
-			// We must consider the scrollbar size of around 20 pixels
-			if ( y <= 0 || y >= window.innerHeight - 20 ) {
-				y -= scrollTo( undefined, y + window.scrollY, true )[ 1 ];
-			}
-			if ( x <= 0 || x >= window.innerWidth - 20 ) {
-				x -= scrollTo( x + window.scrollX, undefined, true )[ 0 ];
+			if ( scroll ) {
+				if ( y <= 0 || y >= window.innerHeight - scrollbarSize ) {
+					y -= scrollTo( undefined, y + window.scrollY, true )[ 1 ];
+				}
+				if ( x <= 0 || x >= window.innerWidth - scrollbarSize ) {
+					x -= scrollTo( x + window.scrollX, undefined, true )[ 0 ];
+				}
+			} else {
+				if (
+					y <= 0 || y >= window.innerHeight - scrollbarSize ||
+					x <= 0 || x >= window.innerWidth - scrollbarSize
+				) {
+					throw new Error( 'Assertion' );
+				}
 			}
 
 			found = document.elementFromPoint( x, y );
--- inputs.js
+++ inputs.js
@@ -7,12 +7,63 @@ function rand( v ) {
 }
 
 module.exports = {
-	async mouseclick( page, x, y ) {
-		logging.info( `Mouse click x=${x}, y=${y}` );
-		util.wait( rand( 500 ) );
+	async mouseclick( page, x, y, type = 'click' ) {
+		logging.info( `Mouse click {x:${x}, y:${y}}` );
+		await util.wait( rand( 500 ) );
 		page.sendEvent( 'mousemove', x, y );
-		util.wait( rand( 50 ) );
-		page.sendEvent( 'click', x, y );
+		await util.wait( rand( 50 ) );
+		page.sendEvent( type, x, y );
+	},
+	async mousedrag( page, ix, iy, fx, fy, movespeed = 1 ) {
+		[ ix, iy, fx, fy ] = [ ix, iy, fx, fy ].map( Math.round );
+		function rtheta( x, y ) {
+			return [
+				Math.sqrt( Math.pow( fx - x, 2 ) + Math.pow( fy - y, 2 ) ),
+				Math.atan2( fy - y, fx - x )
+			];
+		}
+		function angleAvg( t1, w1, t2, w2 ) {
+			let y = w1 * Math.sin( t1 ) + w2 * Math.sin( t2 ),
+				x = w1 * Math.cos( t1 ) + w2 * Math.cos( t2 );
+			return Math.atan2( y, x );
+		}
+
+		logging.info( `Mouse drag {i:{x:${ix}, y:${iy}, f:{x:${fx}, y:${fy}}}` );
+		await util.wait( rand( 500 ) );
+		let [ fd, randtheta ] = rtheta( ix, iy ),
+			[ x, y ] = [ ix, iy ];
+		randtheta += statistics.randomBoxMuller()[ 0 ] / 16;
+
+		page.sendEvent( 'mousedown', x, y );
+		await util.wait( rand( 100 ) );
+
+		while ( Math.round( x ) !== fx || Math.round( y ) !== fy ) {
+			await util.wait( 10 );
+
+			let [ ox, oy ] = [ x, y ],
+				[ d, theta ] = rtheta( x, y ),
+				// proportionLeft = Math.max( 1.05 * ( d / fd ) - 0.05, 0 ),
+				proportionLeft = d / fd,
+				gtheta = angleAvg( randtheta, Math.pow( proportionLeft, 2 ), theta, 1 - Math.pow( proportionLeft, 2 ) ),
+				speed = Math.max( 0.25, ( 1 - Math.pow( 2 * proportionLeft - 1, 2 ) ) * movespeed * Math.pow( Math.log( fd ), 2 ) );
+
+			// logging.error( [ randtheta, Math.pow( proportionLeft, 2 ), theta, 1 - Math.pow( proportionLeft, 2 ) ] );
+			[ x, y ] = [ x + speed * Math.cos( gtheta ), y + speed * Math.sin( gtheta ) ];
+			randtheta += statistics.randomBoxMuller()[ 0 ] / 16 / Math.log( fd );
+
+			// if ( proportionLeft < 0.5 ) {
+			// 	x = Math.abs( x - fx ) < speed / 2 ? fx : x;
+			// 	y = Math.abs( y - fy ) < speed / 2 ? fy : y;
+			// }
+
+			if ( Math.round( x ) !== Math.round( ox ) || Math.round( y ) !== Math.round( oy ) ) {
+				// logging.warn( [ Math.round( x ), Math.round( y ), gtheta, speed ] );
+				page.sendEvent( 'mousemove', Math.round( x ), Math.round( y ) );
+			}
+		}
+
+		await util.wait( rand( 100 ) );
+		page.sendEvent( 'mouseup', fx, fy );
 	},
 	async typetext( page, text ) {
 		if ( typeof text[ Symbol.iterator ] === 'function' ) {
